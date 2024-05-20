79e30d5ab589fa43b1c665b7918fbcd0b0f84d18
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-20 21:04:18 +0200

    Initial commit

---
+++ src/main/java/org/ericwubbo/restcontrolleradvicedemo/RestControllerAdviceDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.restcontrolleradvicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControllerAdviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControllerAdviceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ericwubbo/restcontrolleradvicedemo/RestControllerAdviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.restcontrolleradvicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControllerAdviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
